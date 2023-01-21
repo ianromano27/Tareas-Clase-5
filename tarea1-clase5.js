fd4dc1ecf93e794395c034debe1b22dff8c76da2
ianromano27 <ianromano27@gmail.com>
2023-01-21 16:48:53 -0300

    Tarea1-Clase5
    Se adjunta la tarea 1 de la clase 5 con sus archivos .HTML y .js

---
+++ tarea1-clase5.js
@@ -0,0 +1,16 @@
+const botoncalculo = document.querySelector('#calcular-salario-mensual')
+
+
+botoncalculo.onclick = function () {
+    const salarioAnual = Number(document.querySelector('#salario-anual').value)
+    const salarioMensual = calcularSalarioMensual(salarioAnual)
+    document.querySelector('#salario-mensual').value = salarioMensual
+    return false
+}
+
+
+
+
+function calcularSalarioMensual(salarioAnual) {
+    return salarioAnual / 12
+}
